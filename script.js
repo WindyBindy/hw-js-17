09be474b09efc17ce8f2f7b3d9d74306bdd5ffc7
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-07-19 07:05:20 +0200

    kl;as

---
+++ script.js
@@ -0,0 +1,12 @@
+//  2задание
+const imgEl = document.getElementById('f')
+imgEl.src = 'https://images.prom.ua/6552777201_w600_h600_6552777201.jpg'
+// 3задание
+const link = document.querySelector('link')
+const imgELem = document.getElementById('imgAlt')
+imgElem.alt = 'the best picture'
+link.href = 'https://github.com/'
+//4задание
+const list = document.getElementById('list')
+const firstItem = list.querySelector('li')
+firstItem.textContent = 'text'
